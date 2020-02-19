17e4f1d9cb4f1b7aa89679d3e8695d5776801a0f
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-02-19 14:34:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('div .target');
+}
+
+function increaseRankBy(n) {
+  const ranks = document.getElementById('app').querySelectorAll('.ranked-list li');
+
+  for (let i = 0; i < ranks.length; i++) {
+    let upranked = parseInt(ranks[i]) + n;
+    ranks[i].innerHTML = upranked.toString();
+  }
+}
+
+function deepestChild()
